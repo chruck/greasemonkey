8f190fb93a5d3cd7702eabd803c0f19ae89abbfa
Jas Eckard <eckard@clemson.edu>
2017-08-16 13:30:53 -0400

    Added a generic username/password pre-fill-in script, for Dell DRACs

---
+++ gm_scripts/DRACUserPass.user.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name        DRACUserPass
+// @namespace   DRACs
+// @description login to DRAC
+// @include     https://drac-sp/login.html
+// @version     1
+// @grant       none
+// ==/UserScript==
+
+username="myRoot";
+passwd="myPass";
+
+user.value=username;
+password.value=passwd;
