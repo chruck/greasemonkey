b4711d3fca1cdf1a3e76b7b0e4f1de3f37be4d2e
Jas Eckard <jas+git@eckard.com>
2018-02-05 19:21:32 -0500

    Change to generic JS function

--- gm_scripts/TigerTracks/StickyControlPanel.user.js
+++ gm_scripts/TigerTracks/StickyControlPanel.user.js
@@ -3,10 +3,12 @@
 // @namespace   tigertracks
 // @description This makes the "control panel" (edit/link/subtask) stay at the top when scrolling
 // @include     https://ithelp.clemson.edu/MRcgi/MRTicketPage.pl*
-// @version     2
+// @version     3
 // @grant       GM_addStyle
+// @author	Jas Eckard
 // ==/UserScript==
 
+/*
 GM_addStyle( "                          \
 #controlPanelHolder, #StaticAreaDiv {   \
         position: sticky !important;    \
@@ -18,3 +20,33 @@ GM_addStyle( "                          \
         float: right;                   \
 }                                       \
              ");
+*/
+
+function addGlobalStyle(css)
+{
+    var head, style;
+    head = document.getElementsByTagName('head')[0];
+    if (!head) { return; }
+    style = document.createElement('style');
+    style.type = 'text/css';
+    style.innerHTML = css;
+    head.appendChild(style);
+}
+
+(function()
+{
+    'use strict';
+
+    // Your code here...
+
+    addGlobalStyle('#controlPanelHolder, #StaticAreaDiv { '
+                   +     'position: sticky !important; '
+                   +     'top: 0px; '
+                   +     'background-color: black; '
+                   +     'z-index: 10; '
+                   + '} '
+                   + '#StaticAreaDiv { '
+                   +     'float: right; '
+                   + '}'
+                  );
+})();
